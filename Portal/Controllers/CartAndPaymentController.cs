397d0501b12b5415915c39da0fb5b509c76c7b87
Myron William Echenim <myron.echenim@live.com>
2019-03-14 15:50:15 +0100

    updare

--- Portal/Controllers/CartAndPaymentController.cs
+++ Portal/Controllers/CartAndPaymentController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Portal.Business.Contracts;
+using Portal.Business.StoreManagers;
 using Portal.Business.ViewModels;
 using Portal.Domain.Models;
 using Portal.Domain.Models.Identity;
@@ -47,6 +48,7 @@ namespace Portal.Controllers
             IPasswordHasher<ApplicationUser> passwordHasher,
             IHostingEnvironment hostingEnvironment,
             IArmOneManager armOneManager,
+            IArmOneServiceConfigManager armOneServiceConfigManager,
 
             IMemoryCache cache)
         {
