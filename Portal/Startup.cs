473c9f80165a287be772540796cc4ffbff1b9e8a
Gbadebo Anyangbile <Gbadebo.Anyangbile@arm.com.ng>
2019-06-15 16:24:25 +0100

    merge and sync with remote version

--- Portal/Startup.cs
+++ Portal/Startup.cs
@@ -109,7 +109,7 @@ namespace Portal
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             loggerFactory.AddFile("logs/armclientportal-{Date}.txt");
 
             app.UseHttpsRedirection();
